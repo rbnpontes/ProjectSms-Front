e3bc2ae3b08a68dd3e8aa49ad354713a81bb0c6e
Ruben Gomes <rbnpontes@gmail.com>
2020-02-10 06:28:46 -0300

    Fixed Issues

--- src/components/PlaceholderContact.tsx
+++ src/components/PlaceholderContact.tsx
@@ -1,24 +1,32 @@
 import React from 'react';
-
-export class PlaceholderMessage extends React.Component{
-    render(){
+import '../assets/components/PlaceholderContact.scss';
+export class PlaceholderMessage extends React.Component {
+    render() {
         return (
             <div className="placeholder">
                 <div className="d-flex">
                     <div className="profile-img"></div>
-                    <div className="col mx-0">
-                        John Doe
+                    <div className="col pr-0 pl-2 d-flex align-items-center contact-field">
+                        <section>
+                            <div className="contact-name"><b>John Doe</b></div>
+                            <div className="contact-message">Lorem Ipsum Dollor sit Amet</div>
+                        </section>
                     </div>
-                    <div className="col-1">
-
+                    <div className="d-flex justify-content-center">
+                        <section>
+                            <small>Yesterday</small>
+                            <div className="text-center menu-options">
+                                <i className="fas fa-chevron-down"></i>
+                            </div>
+                        </section>
                     </div>
                 </div>
             </div>
         );
     }
 }
-export default class PlaceholderContact extends React.Component{
-    render(){
+export default class PlaceholderContact extends React.Component {
+    render() {
         return <></>;
     }
 }
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,25 +2,36 @@ import React from 'react';
 import '../assets/pages/Main.scss';
 import { MainLeftNav, MainRightNav } from './Main/NavHeaders';
 import Search from './Main/Search';
+import { PlaceholderMessage } from '../components/PlaceholderContact';
 export default class Main extends React.Component {
+    private get messages() {
+        let arr = new Array(10);
+        for (let i = 0; i < arr.length; i++)
+            arr[i] = i;
+        return arr;
+    }
     render() {
         return (
             <div className="main">
                 <section>
                     <div className="w-100 mx-0 row top-header">
                         <div className="col-4 px-0">
-                            <MainLeftNav/>
+                            <MainLeftNav />
                         </div>
                         <div className="col px-0">
-                            <MainRightNav/>
+                            <MainRightNav />
                         </div>
                     </div>
                     <div className="w-100 mx-0 row">
                         <div className="col-4 px-0">
-                            <Search/>
+                            <Search />
+                            <div className="conversation-field">
+                                {this.messages.map(x => (
+                                    <PlaceholderMessage />
+                                ))}
+                            </div>
                         </div>
                         <div className="col px-0">
-
                         </div>
                     </div>
                 </section>
